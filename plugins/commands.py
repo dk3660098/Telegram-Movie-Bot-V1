5ab5a61c63df7aec500d23e275c0dae3ed7ee2d4
senula1 <89791956+senula1@users.noreply.github.com>
2021-12-29 12:37:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,7 +27,7 @@ async def start(client, message):
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(Script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), disable_web_page_preview=True, reply_markup=reply_markup)
-        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        await asyncio.sleep(2) # 😢 https://github.com/senula1/Tgmoviebot/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, Script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
@@ -45,7 +45,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('search here movie', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('updates', url='https://t.me/josprojects'),
+            InlineKeyboardButton('updates', url='https://t.me/Film_World_Main_Channel'),
             InlineKeyboardButton('movie club', url='https://t.me/WorldFilmGroupp')
             ],[
             InlineKeyboardButton('✗ Close the Menu ✗', callback_data='close_data')
@@ -92,7 +92,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('search here movie', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('updates', url='https://t.me/josprojects'),
+            InlineKeyboardButton('updates', url='https://t.me/Film_World_Main_Channel'),
             InlineKeyboardButton('movie club', url='https://t.me/WorldFilmGroupp')
             ],[
             InlineKeyboardButton('✗ Close the Menu ✗', callback_data='close_data')
